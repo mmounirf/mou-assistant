b359d72cde29ea1faf16d881218f042383ec4371
Mohamed Mounir <m.mounir.f@gmail.com>
2019-10-21 09:24:55 +0200

    Updates

---
+++ precache-manifest.bb79d434e3e0e2e04282c00fe1e20d7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664eb79ba663118f2a1134a6ce6e1d21",
+    "url": "/mou-assistant/index.html"
+  },
+  {
+    "revision": "e7fd97e77e5ac7976db4",
+    "url": "/mou-assistant/static/css/main.62422efa.chunk.css"
+  },
+  {
+    "revision": "2103492b575978c42f80",
+    "url": "/mou-assistant/static/js/2.64ac5bf0.chunk.js"
+  },
+  {
+    "revision": "e7fd97e77e5ac7976db4",
+    "url": "/mou-assistant/static/js/main.3a76693d.chunk.js"
+  },
+  {
+    "revision": "712ad7a36e234d2e25d5",
+    "url": "/mou-assistant/static/js/runtime-main.cd14f141.js"
+  }
+]);
